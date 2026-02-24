5718d30b5d9c6ee0400fc4c9180cfb944a29a0c8
feltspato <227064750+feltspato@users.noreply.github.com>
2026-02-24 18:34:43 -0400

    testing js button

--- testing/script.js
+++ testing/script.js
@@ -1,2 +1,10 @@
 console.log("Hello, World!")
 
+function updateName() {
+    const name = prompt("Enter a new name");
+    button.textContent = "profile name: ${name}";
+}
+
+const button = document.querySelector(".section-two button");
+
+button.addEventListener("click", updateName);
