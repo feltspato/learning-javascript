25343e1c1487189212257984a6932fa8ac41f41f
feltspato <227064750+feltspato@users.noreply.github.com>
2026-02-25 22:04:32 -0400

    adding js button

--- testing/script.js
+++ testing/script.js
@@ -2,7 +2,7 @@ console.log("Hello, World!")
 
 function updateName() {
     const name = prompt("Enter a new name");
-    button.textContent = "profile name: ${name}";
+    button.textContent = `profile name: ${name}`;
 }
 
 const button = document.querySelector(".section-two button");
